fa45b43b84a03b7df5908e26b28e0c7de2741497
_yoonyoon_1 <heeyoon1302@naver.com>
2023-02-23 03:37:52 +0900

    [fix] vote api 수정

--- data_vote.ts
+++ data_vote.ts
@@ -12,4 +12,6 @@ export interface Vote {
   anonymousVote: boolean;
   includingDeadline: boolean;
   deadline: Date;
+  creator : String;
+  createDate : Date;
 }
--- pages/api/vote/index.ts
+++ pages/api/vote/index.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Vote } from '../../../data_vote';
 
 const day = 24 * 60 * 60 * 1000;
 const hour = 60 * 60 * 1000;
@@ -8,14 +9,15 @@ interface Data {
   title: string;
   desc: string | null;
   categories: {
-    name: string;
-    memberCount: number;
+      name: string;
+      member: { id: number; name: string; profileImage: string }[];
   }[];
-
   multiSelection: boolean;
   anonymousVote: boolean;
   includingDeadline: boolean;
   deadline: Date;
+  creator : String;
+  createDate : Date;
 }
 
 const data: Data[] = [
@@ -24,19 +26,22 @@ const data: Data[] = [
     title: '김희윤 죽일까?',
     desc: '김희윤을 죽여야 하는 이유는 명백하다. 김희윤은 적폐다. 대한민국의 4대악 중 하나이다. 나는 그를 죽이고 싶다. 당신들은 내 의견에 동의하는가?',
     categories: [
-      {
-        name: '찬성',
-        memberCount: 20,
-      },
-      {
-        name: '반대',
-        memberCount: 4,
-      },
+        {
+          name: '찬성',
+          member: [{id:1, name:"김희윤", profileImage:"없어"},{id:2, name:"탑쥐수", profileImage:"없어"}]
+        },
+        {
+          name: '반대',
+          member: [{id:1, name:"김희윤", profileImage:"없어"}]
+        }
     ],
     multiSelection: false,
     anonymousVote: false,
     includingDeadline: true,
     deadline: new Date(new Date().getTime() + day * 20 + hour * 10),
+    creator : '송하영',
+    createDate : new Date(new Date().getTime() + day * 20 + hour * 10)
+    
   },
   {
     voteId: 2,
@@ -45,17 +50,20 @@ const data: Data[] = [
     categories: [
       {
         name: '찬성',
-        memberCount: 30,
+        member: [{id:1, name:"김희윤", profileImage:"없어"}]
       },
       {
         name: '반대',
-        memberCount: 2,
-      },
-    ],
+        member: [{id:1, name:"김인직", profileImage:"없어"}]
+      }
+  ],
     multiSelection: false,
     anonymousVote: true,
     includingDeadline: false,
     deadline: new Date(new Date().getTime() + day * 20 + hour * 10),
+    creator : '송하영',
+    createDate : new Date(new Date().getTime() + day * 20 + hour * 10)
+
   },
   {
     voteId: 3,
@@ -63,30 +71,21 @@ const data: Data[] = [
     desc: null,
     categories: [
       {
-        name: '양자강',
-        memberCount: 15,
+        name: '라면',
+        member: [{id:1, name:"김희윤", profileImage:"없어"},{id:2, name:"탑쥐수", profileImage:"없어"}]
       },
       {
-        name: '김밥천국',
-        memberCount: 0,
-      },
-      {
-        name: '돈까스',
-        memberCount: 5,
-      },
-      {
-        name: '라멘',
-        memberCount: 11,
-      },
-      {
-        name: '칼국수',
-        memberCount: 1,
-      },
-    ],
+        name: '국밥',
+        member: [{id:1, name:"송하영", profileImage:"없어"}]
+      }
+  ],
     multiSelection: true,
     anonymousVote: false,
     includingDeadline: true,
     deadline: new Date(new Date().getTime() + day * 10 + hour * 8),
+    creator : '송하영',
+    createDate : new Date(new Date().getTime() + day * 20 + hour * 10)
+
   },
   {
     voteId: 4,
@@ -95,21 +94,24 @@ const data: Data[] = [
     categories: [
       {
         name: '10시',
-        memberCount: 0,
+        member: [{id:1, name:"김희윤", profileImage:"없어"},{id:2, name:"탑쥐수", profileImage:"없어"}]
       },
       {
         name: '10시반',
-        memberCount: 3,
+        member: [{id:1, name:"김희윤", profileImage:"없어"}]
       },
       {
-        name: '11시',
-        memberCount: 3,
+        name: '12시반',
+        member: [{id:1, name:"김희윤", profileImage:"없어"}]
       },
     ],
     multiSelection: false,
     anonymousVote: false,
     includingDeadline: false,
     deadline: new Date(new Date().getTime() + day * 20 + hour * 10),
+    creator : '송하영',
+    createDate : new Date(new Date().getTime() + day * 20 + hour * 10)
+
   },
   {
     voteId: 5,
@@ -118,29 +120,31 @@ const data: Data[] = [
     categories: [
       {
         name: '대마초',
-        memberCount: 20,
+        member: [{id:1, name:"김희윤", profileImage:"없어"}]
       },
       {
         name: '코카인',
-        memberCount: 30,
+        member: [{id:1, name:"김희윤", profileImage:"없어"},{id:2, name:"탑쥐수", profileImage:"없어"}]
       },
       {
         name: '필로폰',
-        memberCount: 1,
+        member: [{id:1, name:"김희윤", profileImage:"없어"}]
       },
       {
         name: '펜타닐',
-        memberCount: 7,
+        member: [{id:1, name:"김희윤", profileImage:"없어"}]
       },
       {
         name: '환각 버섯',
-        memberCount: 12,
+        member: [{id:1, name:"김희윤", profileImage:"없어"},{id:2, name:"탑쥐수", profileImage:"없어"}]
       },
     ],
     multiSelection: true,
     anonymousVote: true,
     includingDeadline: false,
     deadline: new Date(new Date().getTime() + day * 20 + hour * 10),
+    creator : '송하영',
+    createDate : new Date(new Date().getTime() + day * 20 + hour * 10)
   },
 ];
 
